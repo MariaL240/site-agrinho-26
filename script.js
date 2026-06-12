7529aa669f5d074983d6da09caebe762809c1262
MariaL240 <maria.durat@escola.pr.gov.br>
2026-06-11 21:53:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function mostrarSecao(id) {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
 }
+function mostrarSecao(id) {
+
+  const secoes = document.querySelectorAll(".secao");
+
+  secoes.forEach(secao => {
+    secao.style.display = "none";
+  });
+
+  document.getElementById(id).style.display = "block";
+}
